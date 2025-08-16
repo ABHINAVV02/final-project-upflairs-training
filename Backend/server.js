ca86c1b8c6e67acf1c749e846fb0cd861508a730
ABHINAVV02 <abhisharma.v04@gmail.com>
2025-08-16 16:59:55 +0530

    code updated

--- Backend/server.js
+++ Backend/server.js
@@ -155,4 +155,4 @@ app.get("/api/user/stats", basicAuth, (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
